585c42586ed1bfb7b86923dfaa36f53998fd2bea
Daniel Lengyel <daniel.lengyel5@gmail.com>
2021-10-30 12:19:41 +0200

    use imageio to generate gif with all frames

---
+++ main.py
@@ -0,0 +1,14 @@
+import imageio
+
+def main():
+    print("Starting...")
+    reader = imageio.get_reader('sample.mp4')
+
+    with imageio.get_writer('sample.gif', mode="I") as writer:
+        for (i, img) in enumerate(reader):
+            print(f"Frame #{i}")
+            writer.append_data(img)
+
+
+if __name__ == "__main__":
+    main()
