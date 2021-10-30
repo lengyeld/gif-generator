50f78821ca203c30098447ba5a2db75846a4e612
Daniel Lengyel <daniel.lengyel5@gmail.com>
2021-10-30 12:37:58 +0200

    add black formatter

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import imageio
 
+
 def main():
     print("Starting...")
-    reader = imageio.get_reader('sample.mp4')
+    reader = imageio.get_reader("sample.mp4")
 
-    with imageio.get_writer('sample.gif', mode="I") as writer:
+    with imageio.get_writer("sample.gif", mode="I") as writer:
         for (i, img) in enumerate(reader):
             print(f"Frame #{i}")
             writer.append_data(img)
